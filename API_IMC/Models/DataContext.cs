9e668483c9aeabfb0e259f1640763e4580859e8e
amanda <amanda.dias.t_stefaninni@grupoboticario.com.br>
2022-12-08 19:52:40 -0300

    a

---
+++ API_IMC/Models/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Imc> Imcs { get ; set; }
+    }
+}
